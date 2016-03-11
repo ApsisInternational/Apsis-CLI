488962d49348105f5bc76b17439828f69ce6b3a4
Hampus Persson <h@hmps.se>
2016-03-11 12:23:41 +0100

    fix: remove console.log

--- src/index.js
+++ src/index.js
@@ -5,15 +5,4 @@ const runCommand = require('./scripts/run-command.js');
 const menu = require('./scripts/cli-menu.js');
 const cmd = args[0];
 
-console.log(runCommand)
 runCommand(cmd);
-
-module.exports = main;
-
-function main(cmd) {
-    if (cmd && cmd.length) {
-        runCommand(cmd);
-    } else {
-        menu();
-    }
-}
